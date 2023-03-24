032abd022d56bd6fc105499acc0d4f0e5a969ec2
Luka Novsak <lnovsak@gmail.com>
2023-03-24 18:49:54 +0100

    make tables sortable

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,9 +1,18 @@
 class TopicsController < ApplicationController
   def index
+    @sort_column =
+      %w[remote_created_at title posts_count last_post_remote_created_at]
+        .include?(params[:sort]) ?
+        params[:sort] : "last_post_remote_created_at"
+
+    @sort_direction =
+      %w[asc desc].include?(params[:sort_direction]) ?
+        params[:sort_direction] : "desc"
+
     @topics =
       Topic
         .preload(:user, last_post: :user)
-        .order(last_post_remote_created_at: :desc)
+        .order("#@sort_column #@sort_direction")
         .page(params[:page]).per(50)
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
   def index
-    @users = User.order(:name).page(params[:page]).per(30)
+    @sort_column =
+      %w[name posts_count topics_count messages_count]
+        .include?(params[:sort]) ?
+        params[:sort] : "name"
+
+    @sort_direction =
+      %w[asc desc].include?(params[:sort_direction]) ?
+        params[:sort_direction] : "asc"
+
+    @users =
+      User
+        .order("#@sort_column #@sort_direction")
+        .page(params[:page]).per(30)
   end
 
   def show
