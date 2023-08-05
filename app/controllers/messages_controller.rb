11f7d6910d09bdaa0a6f3934c5f2942a99aa7665
Luka Novsak <lnovsak@gmail.com>
2023-08-05 02:09:54 +0200

    add sorting of messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,18 @@
 class MessagesController < ApplicationController
   def index
+    @sort_column =
+      %w[remote_created_at]
+        .include?(params[:sort]) ?
+        params[:sort] : "remote_created_at"
+
+    @sort_direction =
+      %w[asc desc].include?(params[:sort_direction]) ?
+        params[:sort_direction] : "desc"
+
     @messages =
       Message
         .preload(:user)
-        .order(remote_created_at: :desc)
+        .order("#{@sort_column} #{@sort_direction}")
         .page(params[:page]).per(30)
   end
 end
